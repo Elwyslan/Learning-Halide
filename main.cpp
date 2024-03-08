a041e89c2ada86cc82319c4b16f7f62712dcb6c2
Elwys <Elwys@ELWYSLAN>
2024-03-08 00:46:00 -0300

    Adicionar arquivos de projeto.

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <Halide.h>
+
+
+using namespace std;
+int main(int argc, char* argv[]) {
+
+	Halide::Func gradient;
+
+	Halide::Var x, y;
+
+	Halide::Expr e = x + y;
+
+	gradient(x, y) = e;
+
+	Halide::Buffer<int32_t> output = gradient.realize({ 800, 600 });
+
+    for (int j = 0; j < output.height(); j++) {
+        for (int i = 0; i < output.width(); i++) {
+            // We can access a pixel of an Buffer object using similar
+            // syntax to defining and using functions.
+            if (output(i, j) != i + j) {
+                printf("Something went wrong!\n"
+                    "Pixel %d, %d was supposed to be %d, but instead it's %d\n",
+                    i, j, i + j, output(i, j));
+                return -1;
+            }
+        }
+    }
+
+	cout << "########\nFim!" << endl;
+	return 0;
+}
